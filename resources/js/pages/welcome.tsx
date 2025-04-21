d2bc3e86c0b6bc3bf4d38e90ec041ef96ecace56
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2025-04-21 14:10:14 -0300

    welcome 0.3.6

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -102,6 +102,7 @@ export default function Welcome() {
                                 {paragraph}
                             </p>
                         ))}
+                        <span className="blinking-cursor"></span>
                     </div>
 
                     {/* Footer */}
