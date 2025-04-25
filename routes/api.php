2608b073559b4cbdd106b20bc4ffedea195bac27
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2025-04-25 14:44:11 -0300

    dashboard messages 0.1.9

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,6 @@
 <?php
 
 use App\Http\Controllers\MessageController;
-use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::post('/api/messages/{id}/toggle-read', [MessageController::class, 'toggleReadStatus']);
-
+Route::post('/messages/{id}/toggle-read', [MessageController::class, 'toggleRead']);
