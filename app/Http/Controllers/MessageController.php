3987f20f6d9bc8d60d0bf9b2bbe21faef64582ea
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2025-04-25 09:17:42 -0300

    dashboard messages 0.0.3

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
 
 class MessageController extends Controller
 {
