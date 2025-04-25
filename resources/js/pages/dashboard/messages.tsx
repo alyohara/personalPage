bdc5a23939b28f673de919aa66036977d27e8799
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2025-04-25 09:27:24 -0300

    dashboard messages 0.0.4

--- resources/js/pages/dashboard/messages.tsx
+++ resources/js/pages/dashboard/messages.tsx
@@ -1,4 +1,3 @@
-import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
@@ -45,7 +44,7 @@ export default function Messages({ messages }: Props) {
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3"></div>
                 <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min">
-                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    {/*<PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />*/}
 
                     <div className="p-6">
                         <h1 className="mb-4 text-2xl font-bold">Mensajes</h1>
@@ -74,7 +73,7 @@ export default function Messages({ messages }: Props) {
                                 <button
                                     key={index}
                                     onClick={() => handlePageChange(link.url)}
-                                    className={`border px-4 py-2 ${link.active ? 'bg-blue-500 text-white' : 'bg-white text-blue-500'}`}
+                                    className={`rounded-lg border px-4 py-2 ${link.active ? 'bg-sidebar-border text-white' : 'text-sidebar-border bg-white'}`}
                                     dangerouslySetInnerHTML={{ __html: link.label }}
                                 />
                             ))}
