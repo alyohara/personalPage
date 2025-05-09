3eacff7f930ce40274e1aad0033190c127c60d9c
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2025-05-09 13:47:22 -0300

    posts edit image

--- database/migrations/2025_05_09_164644_change_content_column_type_in_posts_table.php
+++ database/migrations/2025_05_09_164644_change_content_column_type_in_posts_table.php
@@ -6,23 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->longText('content')->change();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->text('content')->change();
         });
     }
 };
