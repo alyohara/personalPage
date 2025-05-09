1cae7f932f35239af0393cae4844760c12334b67
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2025-05-09 14:47:34 -0300

    new table posts adecuation

--- database/migrations/2025_05_09_174826_change_content_column_to_longtext_in_posts_table.php
+++ database/migrations/2025_05_09_174826_change_content_column_to_longtext_in_posts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->longText('content')->change();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->text('content')->change();
         });
     }
 };
