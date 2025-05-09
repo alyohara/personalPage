23248ce47b28e843f26deeadee943567363c2028
Angel Leonardo Bianco <angel.leonardo.bianco@gmail.com>
2025-05-09 13:35:06 -0300

    posts edit 0.2.0

--- resources/js/pages/dashboard/post-edit.tsx
+++ resources/js/pages/dashboard/post-edit.tsx
@@ -176,13 +176,49 @@ export default function PostEdit({ post }: Props) {
                                 height: 500,
                                 menubar: true,
                                 plugins: [
-                                    'advlist', 'autolink', 'lists', 'link', 'image', 'charmap',
-                                    'preview', 'anchor', 'searchreplace', 'visualblocks', 'code',
-                                    'fullscreen', 'insertdatetime', 'media', 'table', 'help', 'wordcount', 'codesample'
+                                    'advlist',
+                                    'autolink',
+                                    'lists',
+                                    'link',
+                                    'image',
+                                    'charmap',
+                                    'preview',
+                                    'anchor',
+                                    'searchreplace',
+                                    'visualblocks',
+                                    'code',
+                                    'fullscreen',
+                                    'insertdatetime',
+                                    'media',
+                                    'table',
+                                    'code',
+                                    'help',
+                                    'wordcount',
                                 ],
-                                toolbar: 'undo redo | formatselect | bold italic backcolor | \
+                                toolbar:
+                                    'undo redo | formatselect | bold italic backcolor | \
                                     alignleft aligncenter alignright alignjustify | \
-                                    bullist numlist outdent indent | removeformat | help | codesample | link image media'
+                                    bullist numlist outdent indent | removeformat | help | \
+                                    link image media codesample',
+                                    image_title: true,
+                                    automatic_uploads: true,
+                                    file_picker_types: 'image',
+                                    file_picker_callback: (callback, value, meta) => {
+                                        if (meta.filetype === 'image') {
+                                            const input = document.createElement('input');
+                                            input.setAttribute('type', 'file');
+                                            input.setAttribute('accept', 'image/*');
+                                            input.onchange = function () {
+                                                const file = this.files[0];
+                                                const reader = new FileReader();
+                                                reader.onload = function () {
+                                                    callback(reader.result, { alt: file.name });
+                                                };
+                                                reader.readAsDataURL(file);
+                                            };
+                                            input.click();
+                                        }
+                                    },
                             }}
                         />
                     </div>
